864b54694290d5a141f6a0a267f0c20ac9292d16
MistyIslam5 <mdmisty5@gmail.com>
2024-09-03 20:54:41 +0600

    recover loop object and array

---
+++ lopping.js
@@ -0,0 +1,36 @@
+/**
+ * 𝗳𝗼𝗿 𝗹𝗼𝗼𝗽 
+ * 𝘄𝗵𝗶𝗹𝗲 𝗹𝗼𝗼𝗽
+ * 𝙛𝙤𝙧 𝙤𝙛 ---> 𝙖𝙧𝙧𝙖𝙮 𝙩𝙚 𝙪𝙨𝙚 𝙠𝙤𝙧𝙖 𝙝𝙤𝙞
+ * 𝙛𝙤𝙧 𝙞𝙣 
+ */
+
+const friends = ['elon', 'bill', 'mark', 'waren'];
+
+for(const friend of friends ){
+    // console.log(friend);
+    
+}
+
+
+for (let i=0; i<friends.length; i++) {
+    // console.log(i);
+    
+    // console.log(friends[i]);
+}
+
+const number = [45,45,67,87,89,34,65,879,9,786,57,353,24];
+
+for (let i=0; i<number.length; i++){
+    // console.log(number[i]);
+}
+
+const names = ['John', 'Alice', 'Bob', 'Emily', 'David'];
+let i =0;
+while(i<names.length){
+    console.log(i);
+    
+    console.log(names[i]);
+    i++;
+    
+}
