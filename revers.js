16006d07205e00da02a73a091f96c8cb36610c75
MistyIslam5 <mdmisty5@gmail.com>
2024-09-03 22:59:19 +0600

    revers

---
+++ revers.js
@@ -0,0 +1,26 @@
+let numbers = [1,2,3,4,5,6,7,8,9]
+// numbers.reverse();
+
+// console.log(numbers) // Output: [9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+
+/**
+ * 𝗮𝘁𝗮 𝗼𝗻𝗲𝗸 𝗴𝘂𝗿𝘂𝘁𝘁𝗼 𝗽𝘂𝗿𝗻𝗼
+ */
+const revNumber = [];
+
+for (const number of numbers) {
+    // console.log(number);
+    revNumber.unshift(number);
+    
+}
+// console.log(revNumber);
+
+const names = ['rahim', 'karim', 'kartik', 'kartik', 'mosfiq'];
+
+names.reverse();
+
+console.log(names);
+
+
+
